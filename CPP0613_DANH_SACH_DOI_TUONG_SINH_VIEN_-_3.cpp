8aa65f75c24eaf128975fe7b3b664dd944367947
baomt-gpt <maithebao19022007@gmail.com>
2026-05-12 23:07:01 +0700

    feat: add GPA sorting for class student list

--- CPP0613_DANH_SACH_DOI_TUONG_SINH_VIEN_-_3.cpp
+++ CPP0613_DANH_SACH_DOI_TUONG_SINH_VIEN_-_3.cpp
@@ -1,3 +1,80 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int cnt = 0;
+
+class SinhVien{
+    private:
+        string maSV, ten, lop, ns;
+        float gpa;
+    public:
+        SinhVien(){
+            maSV = ten = lop = ns = "";
+            gpa = 0;
+        }
+        string ChuanHoa(string);
+        friend ostream& operator << (ostream&, SinhVien);
+        friend istream& operator >> (istream&, SinhVien&);
+        float getGpa(){
+            return gpa;
+        }
+};
+
+bool cmp(SinhVien a, SinhVien b){
+    return a.getGpa() > b.getGpa();
+}
+
+string SinhVien::ChuanHoa(string a){
+    stringstream ss(a);
+    string word, res = "";
+    while(ss >> word){
+        word[0] = toupper(word[0]);
+        for(int i = 1; i < word.size(); i++){
+            word[i] = tolower(word[i]);
+        }
+        res += word + " ";
+    }
+    res.pop_back();
+    return res;
+}
+
+ostream& operator << (ostream& out, SinhVien a){
+    out << a.maSV << " "
+        << a.ten << " "
+        << a.lop << " "
+        << a.ns << " "
+        << fixed << setprecision(2) << a.gpa << "\n";
+    return out;
+}
+
+istream& operator >> (istream& in, SinhVien &a){
+    ++cnt;
+    a.maSV = "B20DCCN";
+    string tmp = to_string(cnt);
+    while(tmp.size() < 3) tmp = "0" + tmp;
+    a.maSV = a.maSV + tmp;
+    getline(in >> ws, a.ten);
+    a.ten = a.ChuanHoa(a.ten);
+    in >> a.lop >> a.ns >> a.gpa;
+    if(a.ns[2] != '/') a.ns = "0" + a.ns;
+    if(a.ns[5] != '/') a.ns.insert(3, "0");
+    return in;
+}
+
+void sapxep(SinhVien a[], int n){
+    sort(a, a + n, cmp);
+}
+
+int main(){
+    SinhVien ds[50];
+    int N, i;
+    cin >> N;
+    for(i=0;i<N;i++){
+        cin >> ds[i];
+    }
+    sapxep(ds, N);
+    for(i=0;i<N;i++){
+        cout << ds[i];
+    }
+    return 0;
+}
